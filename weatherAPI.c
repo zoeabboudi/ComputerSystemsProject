4a158f85a8fcd33bc1885430f23de27d8c157fe0
Zoe Abboudi <38954728+zoeabboudi@users.noreply.github.com>
2018-12-26 20:09:29 -0500

    Update weatherAPI.c

--- weatherAPI.c
+++ weatherAPI.c
@@ -8,7 +8,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 
+#if 0
 void echo(int connfd) 
 {
     size_t n; 
@@ -32,6 +34,7 @@ int main(int argc, char **argv)
 erveri:sockaddrstorage                                                                         
     char client_hostname[MAXLINE], client_port[MAXLINE];
 
+
     if (argc != 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
@@ -49,20 +52,27 @@ erveri:sockaddrstorage
     }
 
 
+#endif
+int main(void){
     
     char *zipcode, *urlBegin, *urlEnd;
     char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+
     int n1=0, n2=0;
 
     /* Extract the zipCode */
     zipcode = strncat(getenv("QUERY_STRING"), "\0", 5);
+    sprintf(content, "%s \n<p>", zipcode);
+    sprintf(content, "%s and this!");
       // make sure that the arg are actually zipcode
-    urlBegin ="https:api.openweathermap.org/data/2.5/weather?zip=";
-    urlEnd = "&units=imperial&APPID=364ebbea13d49a5e8e505870368c78be";
-    strcat(urlBegin, zipcode);
-    strcat(urlBegin, urlEnd);
-    const char* command  = "wget -O results.txt urlBegin";
-    system(command);
+    //urlBegin ="https:api.openweathermap.org/data/2.5/weather?zip=";
+    //urlEnd = "&units=imperial&APPID=364ebbea13d49a5e8e505870368c78be";
+    //strcat(urlBegin, zipcode);
+    //strcat(urlBegin, urlEnd);
+    // const char* command  = "wget -O results.txt urlBegin";
+    int result = 0;
+    //result = system(command);
+    //fprintf(stderr, "this is what happened: %d", result);
 	
     
       /* Argument list starts with "?"
@@ -75,14 +85,19 @@ erveri:sockaddrstorage
           "sortBy=popularity&" +
           "apiKey=f8f3dee3f2524f47a1309bc2381b8cb2"; */
 
+    
+
+
     /* Make the response body */
-    sprintf(content, "Welcome to Zoe & Mori's News API: ");
+    sprintf(content, "%s this is result");
+    sprintf(content, "%sWelcome to Zoe & Mori's News API: ");
     sprintf(content, "%sZoe & Mori's News portal.\r\n<p>", content);
     //replace next line with how to parse through JSON given and return it nicely
-    sprintf(content, "%sThe answer is: %d \r\n<p>", 
-	    content, n1, n1);
+    //sprintf(content, "%sThe answer is: %d \r\n<p>", 
+    //content, n1, n1);
     sprintf(content, "%sThanks for visiting!\r\n", content);
-  
+
+    
     /* Generate the HTTP response */
     printf("Connection: close\r\n");
     printf("Content-length: %d\r\n", (int)strlen(content));
